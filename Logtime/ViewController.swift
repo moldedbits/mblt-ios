22f9f53de5afcbeee53b2a43d419a7a63b8b1294
Amit Kumar Swami <amitiitm2009@gmail.com>
2017-04-18 17:35:48 +0530

    Initial Commit

---
+++ Logtime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Logtime
+//
+//  Created by Amit kumar Swami on 18/04/17.
+//  Copyright © 2017 molededbits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
